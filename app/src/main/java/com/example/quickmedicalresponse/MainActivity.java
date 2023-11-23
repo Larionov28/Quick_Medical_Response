cd4f20310a908258765df6601fb24664f8867451
Artem Larionov <123487852+Larionov28@users.noreply.github.com>
2023-11-23 09:10:18 +0600

    Fixed warnings

--- app/src/main/java/com/example/quickmedicalresponse/MainActivity.java
+++ app/src/main/java/com/example/quickmedicalresponse/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.quickmedicalresponse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,29 +19,23 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
 
         // Обработчик события для EditText
-        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    // При фокусировке очищаем EditText, если текст "Введи имя" ещё там есть
-                    if (editText.getText().toString().equals("Введи имя")) {
-                        editText.setText("");
-                    }
+        editText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                // При фокусировке очищаем EditText, если текст "Введи имя" ещё там есть
+                if (editText.getText().toString().equals("Введи имя")) {
+                    editText.setText("");
                 }
             }
         });
 
         // Обработчик события для кнопки
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = editText.getText().toString();
+        button.setOnClickListener(v -> {
+            String name = editText.getText().toString();
 
-                // Проверяем, пустое ли поле ввода
-                if (!name.isEmpty()) {
-                    // Выводим текст в TextView с использованием введённого имени
-                    textView.setText("Привет " + name + "!");
-                }
+            // Проверяем, пустое ли поле ввода
+            if (!name.isEmpty()) {
+                // Выводим текст в TextView с использованием введённого имени
+                textView.setText(String.format("Привет '%s' !", name));
             }
         });
     }
