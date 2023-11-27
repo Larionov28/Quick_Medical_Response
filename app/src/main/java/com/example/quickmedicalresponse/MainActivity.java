f90a413488e634ca6de0e6d093a30d702c869042
Artem Larionov <123487852+Larionov28@users.noreply.github.com>
2023-11-27 14:11:17 +0600

    Saving changes

--- app/src/main/java/com/example/quickmedicalresponse/MainActivity.java
+++ app/src/main/java/com/example/quickmedicalresponse/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.quickmedicalresponse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,30 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        final EditText editText = findViewById(R.id.editTextText);
-        final TextView textView = findViewById(R.id.textView);
-        Button button = findViewById(R.id.button);
-
-        // Обработчик события для EditText
-        editText.setOnFocusChangeListener((v, hasFocus) -> {
-            if (hasFocus) {
-                // При фокусировке очищаем EditText, если текст "Введи имя" ещё там есть
-                if (editText.getText().toString().equals("Введи имя")) {
-                    editText.setText("");
-                }
-            }
-        });
-
-        // Обработчик события для кнопки
-        button.setOnClickListener(v -> {
-            String name = editText.getText().toString();
-
-            // Проверяем, пустое ли поле ввода
-            if (!name.isEmpty()) {
-                // Выводим текст в TextView с использованием введённого имени
-                textView.setText(String.format("Привет %s !", name));
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
